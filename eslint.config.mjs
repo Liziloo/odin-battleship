fb8a1346cd0aa6a79d3e5f38ab104fdabe9b06cd
Liz <162842106+Liziloo@users.noreply.github.com>
2025-02-07 10:08:27 -0500

    Incorporate prettier config and configure ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,7 @@ import pluginJs from "@eslint/js";
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+  ignores: ["**/node_modules/", ".git/", "./webpack.common.js", "./webpack.dev.js", "./webpack.prod.js"]
+  }
 ];
