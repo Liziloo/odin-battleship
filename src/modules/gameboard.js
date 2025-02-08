44009b166533545d8ca5ec87c10eaaada59f252c
Liz <162842106+Liziloo@users.noreply.github.com>
2025-02-08 16:15:50 -0500

    feat: write placeShip method for Gameboard class

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -12,10 +12,19 @@ class Gameboard {
         return Array(10).fill(null).map(() => Array(10).fill({hit: false, ship: null}));
     }
 
-    placeShip(x, y, length) {
+    placeShip(x, y, length, direction) {
+        if (x < 0 || y < 0 || x > 9 || y > 9) return false;
         const ship = new Ship(length);
-        for (let i = x - 1; i < ship.length + x - 1; i++) {
-            this.grid[i][y].ship = ship;
+        if (direction === 'horizontal') {
+            for (let i = x; i < ship.length + x; i++) {
+                if (!this.grid[i][y]) return false;
+                this.grid[i][y].ship = ship;
+            }
+        } else {
+            for (let i = y; i < ship.length + y; i++) {
+                if (!this.grid[x][i]) return false;
+                this.grid[x][i].ship = ship;
+            }
         }
     }
 }
--- src/modules/gameboard.test.js
+++ src/modules/gameboard.test.js
@@ -2,9 +2,10 @@ import { Gameboard } from "./gameboard";
 import { Ship } from "./ship";
 import { expect, test } from '@jest/globals';
 
+
 test('places new destroyer on grid', () => {
     const board = new Gameboard;
-    board.placeShip(1, 3, 2);
+    board.placeShip(0, 2, 2, 'horizontal');
     
     expect(board.grid[0][2]).toEqual({hit: false, ship: expect.any(Ship)});
     expect(board.grid[1][2]).toEqual({hit: false, ship: expect.any(Ship)});
@@ -12,9 +13,32 @@ test('places new destroyer on grid', () => {
 
 test('places new submarine on grid', () => {
     const board = new Gameboard;
-    board.placeShip(1, 3, 3);
+    board.placeShip(0, 2, 3, 'horizontal');
     
     expect(board.grid[0][2]).toEqual({hit: false, ship: expect.any(Ship)});
     expect(board.grid[1][2]).toEqual({hit: false, ship: expect.any(Ship)});
     expect(board.grid[2][2]).toEqual({hit: false, ship: expect.any(Ship)});
 })
+
+test('returns false for ship originating outside grid bounds', () => {
+    const board = new Gameboard;
+    expect(board.placeShip(0, 11, 2, 'horizontal')).toBeFalsy();
+});
+
+test('returns false for ship whose length goes out of bounds', () => {
+    const board = new Gameboard;
+    expect(board.placeShip(0, 9, 2, 'horizontal')).toBeFalsy();
+});
+
+test('places a ship vertically', () => {
+    const board = new Gameboard();
+    board.placeShip(0, 2, 2, 'vertical');
+
+    expect(board.grid[0][2]).toEqual({hit: false, ship: expect.any(Ship)});
+    expect(board.grid[0][3]).toEqual({hit: false, ship: expect.any(Ship)});
+})
+
+test('returns false for vertical ship whose length goes out of bounds', () => {
+    const board = new Gameboard;
+    expect(board.placeShip(9, 0, 2, 'vertical')).toBeFalsy();
+});
