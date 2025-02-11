e83d8256db2933847f3cb6d0f4f966acfb78c988
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-02-11 16:19:42 -0500

    Switch workstations

--- src/index.js
+++ src/index.js
@@ -4,61 +4,16 @@ import './styles/comeau-reset.css';
 import './styles/styles.css';
 import { loadWin } from './modules/loadWin.js';
 import { computerAttack } from './modules/computerLogic.js';
+import { placeShips } from './modules/placeShips.js';
 
 const player1 = new Player('real');
 const player2 = new Player('computer');
-
-// Place all ships for both players
-// Patrol boats
-player1.board.placeShip(0, 0, 2, 'horizontal');
-player1.board.placeShip(0, 1, 2, 'horizontal');
-player1.board.placeShip(0, 2, 2, 'horizontal');
-player1.board.placeShip(0, 3, 2, 'horizontal');
-player1.board.placeShip(0, 4, 2, 'horizontal');
-
-player2.board.placeShip(0, 0, 2, 'horizontal');
-player2.board.placeShip(0, 1, 2, 'horizontal');
-player2.board.placeShip(0, 2, 2, 'horizontal');
-player2.board.placeShip(0, 3, 2, 'horizontal');
-player2.board.placeShip(0, 4, 2, 'horizontal');
-
-// Submarines
-player1.board.placeShip(0, 5, 3, 'horizontal');
-player1.board.placeShip(0, 6, 3, 'horizontal');
-player1.board.placeShip(0, 7, 3, 'horizontal');
-player1.board.placeShip(0, 8, 3, 'horizontal');
-
-player2.board.placeShip(0, 5, 3, 'horizontal');
-player2.board.placeShip(0, 6, 3, 'horizontal');
-player2.board.placeShip(0, 7, 3, 'horizontal');
-player2.board.placeShip(0, 8, 3, 'horizontal');
-
-// Destroyers
-player1.board.placeShip(0, 9, 3, 'horizontal');
-player1.board.placeShip(2, 0, 3, 'horizontal');
-player1.board.placeShip(2, 1, 3, 'horizontal');
-
-player2.board.placeShip(0, 9, 3, 'horizontal');
-player2.board.placeShip(2, 0, 3, 'horizontal');
-player2.board.placeShip(2, 1, 3, 'horizontal');
-
-// Battleships
-player1.board.placeShip(2, 2, 4, 'horizontal');
-player1.board.placeShip(2, 3, 4, 'horizontal');
-
-player2.board.placeShip(2, 2, 4, 'horizontal');
-player2.board.placeShip(2, 3, 4, 'horizontal');
-
-// Carriers
-player1.board.placeShip(2, 4, 5, 'horizontal');
-
-player2.board.placeShip(2, 4, 5, 'horizontal');
 
 let currentPlayer = player1;
 let otherPlayer = player2;
 
 (function() {
-    round(currentPlayer, otherPlayer);
+    placeShips(currentPlayer, otherPlayer);
     const enemyBoardDiv = document.querySelector('.enemy-board');
     enemyBoardDiv.addEventListener('click', (e) => {
         if (e.target.classList.contains('cell')) {
