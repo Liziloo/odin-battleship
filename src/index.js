016a2e1c16eb5208a56d6ead2d1f1d1c9ce4351d
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-02-12 16:25:07 -0500

    trying something new

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import { round } from './modules/round.js';
-import { Player } from './modules/player.js';
-import './styles/comeau-reset.css';
-import './styles/styles.css';
-import { loadWin } from './modules/loadWin.js';
-import { computerAttack } from './modules/computerLogic.js';
 import { placeShips } from './modules/placeShips.js';
 
 const player1 = new Player('real');
@@ -13,39 +7,5 @@ let currentPlayer = player1;
 let otherPlayer = player2;
 
 (function() {
-    placeShips(currentPlayer);
-    placeShips(otherPlayer);
-    round(currentPlayer, otherPlayer);
-    const enemyBoardDiv = document.querySelector('.enemy-board');
-    enemyBoardDiv.addEventListener('click', (e) => {
-        if (e.target.classList.contains('cell')) {
-            const xCoord = e.target.dataset.x;
-            const yCoord = e.target.dataset.y;
-            otherPlayer.board.receiveAttack(xCoord, yCoord);
-            if (otherPlayer.board.grid[xCoord][yCoord].ship !== null) {
-                alert("It's a hit!")
-            } else {
-                alert("“Giving up is the only sure way to fail.”")
-            }
-            const finished = otherPlayer.board.allSunk();
-            if (!finished) {
-                if (currentPlayer.type === 'real' && otherPlayer.type === 'real') {
-                    currentPlayer = currentPlayer === player1 ? player2 : player1;
-                    otherPlayer = otherPlayer === player1 ? player2 : player1;
-                    round(currentPlayer, otherPlayer);
-                } else {
-                    const result = computerAttack(currentPlayer.board);
-                    round(currentPlayer, otherPlayer);
-                    if (result === 'hit') {
-                        alert("You've been hit!");
-                    } else {
-                        alert("Evasion effective!")
-                    }
-                }
-            } else {
-                loadWin();
-            }
-        }
-       
-    })
+    const player1 = placeShips();
 })();
